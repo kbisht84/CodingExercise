121009660f68b352da162a7a929849b8c4df9aec
kanak bisht <kanak.bisht@mitel.com>
2020-01-31 07:07:49 -0800

    Adding decode string problem

---
+++ src/string/DecodeString.java
@@ -0,0 +1,41 @@
+package string;
+
+import java.util.Stack;
+
+public class DecodeString {
+    public String decodeString(String s) {
+        Stack<Integer> counts = new Stack();
+        Stack<String>  result = new Stack();
+        String res = "";
+        int index = 0;
+        while(index < s.length()){
+
+            if(Character.isDigit(s.charAt(index))){
+                int count =0;
+                while(Character.isDigit(s.charAt(index))){
+                    count = 10 * count + s.charAt(index) - '0';
+                    index++;
+                }
+                counts.push(count);
+            }
+            else if(s.charAt(index) == '['){
+                result.push(res);
+                res = "";
+                index++;
+
+            }else if(s.charAt(index)  == ']'){
+                StringBuilder sb = new StringBuilder(result.pop());
+                int count = counts.pop();
+                for(int i=0; i<count;i++)
+                    sb.append(res);
+                res = sb.toString();
+                index++;
+            }
+            else{
+                res +=s.charAt(index);
+                index++;
+            }
+        }
+        return res;
+    }
+}
